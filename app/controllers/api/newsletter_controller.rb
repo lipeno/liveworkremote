c600eb20377cb976ec7f1f45435f063d876928cf
Andrej Dragisic <andrejdragisic@gmail.com>
2015-01-04 20:08:15 +0100

    Separate newsletter for production

--- app/controllers/api/newsletter_controller.rb
+++ app/controllers/api/newsletter_controller.rb
@@ -6,6 +6,9 @@ module Api
         email = params[:email]
         mailchimp_api_key = 'ed3fbea391311526c384b43bc1da3fb9-us3'
         list_id = "0fc9a731db"
+        if Rails.env.production?
+          list_id = "9dee4d1aec"
+        end
         double_optin = false
 
         mailchimp = Mailchimp::API.new(mailchimp_api_key)
