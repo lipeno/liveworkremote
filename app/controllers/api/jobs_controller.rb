5921bdc6b76015e2a19193dd73cdac1ec0b41740
Andrej Dragisic <andrejdragisic@gmail.com>
2015-01-06 20:45:05 +0100

    Return only 2 months old jobs

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class JobsController < ApplicationController
     def index
-      @jobs = Job.all.order('published_at DESC')
+      @jobs = Job.all.order('published_at DESC').where(['published_at > ?', DateTime.now - 2.months])
     end
   end
 end
